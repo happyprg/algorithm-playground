142da8dadbfb135b394053217ee0fdc3bc87ed44
hongsgo <hongsgo@gmail.com>
2019-01-30 18:59:58 +0900

    NO-BTS Add Hackerank

---
+++ HackerRank/Diagonal_Difference.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the diagonalDifference function below.
+    static int diagonalDifference(int[][] arr) {
+
+        int leftToRight = 0;
+        int leftToRightIndex = 0;
+        for (int i = 0; i < arr.length; i++) {
+            leftToRight += arr[i][leftToRightIndex++];
+        }
+
+        int rightToLeft = 0;
+        int rightToLeftIndex = arr.length - 1;
+        for (int i = 0; i < arr.length; i++) {
+            rightToLeft += arr[i][rightToLeftIndex--];
+        }
+
+        return Math.abs(leftToRight - rightToLeft);
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[][] arr = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            String[] arrRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < n; j++) {
+                int arrItem = Integer.parseInt(arrRowItems[j]);
+                arr[i][j] = arrItem;
+            }
+        }
+
+        int result = diagonalDifference(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
