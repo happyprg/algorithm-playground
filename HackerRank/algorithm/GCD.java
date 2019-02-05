de3ca0d7e29f99fa54135b8db12b352f5ff42f6e
hongsgo <hongsgo@gmail.com>
2019-02-06 00:58:37 +0900

    NO-BTS solved problem

---
+++ HackerRank/algorithm/GCD.java
@@ -0,0 +1,36 @@
+package algorithm;// IMPORT LIBRARY PACKAGES NEEDED BY YOUR PROGRAM
+
+import org.junit.Test;
+
+// SOME CLASSES WITHIN A PACKAGE MAY BE RESTRICTED
+// DEFINE ANY CLASS AND METHOD NEEDED
+// CLASS BEGINS, THIS CLASS IS REQUIRED
+public class GCD {
+    // METHOD SIGNATURE BEGINS, THIS METHOD IS REQUIRED
+
+    @Test
+    public void generalizedGCD() {
+
+        int num = 5;
+//        int[] arr = new int[]{2, 3, 4, 5, 6};
+        int[] arr = new int[]{2, 4, 6, 8, 10};
+        int result = -1;
+        for (int i = arr.length - 1; i >= 0; i--) {
+            int standard = arr[i];
+            int checkNum = 0;
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[j] % standard == 0) {
+                    checkNum++;
+                }
+                if (checkNum == num) {
+                    result = standard;
+                    break;
+                }
+            }
+        }
+        if (result == -1) {
+            result = 1;
+        }
+        System.out.println("result = " + result);
+    }
+}
