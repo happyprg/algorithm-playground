d7d98ee6e9dac95cad8020e5cac504dc5a0dc54c
jeehong.lee <jeehong.lee@linecorp.com>
2019-03-28 16:54:36 +0900

    NO-BTS solve problem

--- Sort/AlreadySortedSameLengthMerge.java
+++ Sort/AlreadySortedSameLengthMerge.java
@@ -30,38 +30,4 @@ public class AlreadySortedSameLengthMerge {
 
     }
 
-    private static void quickSort(int[] arr, int start, int end) {
-        int part2 = partition(arr, start, end);
-        if (start < part2 - 1) {
-            quickSort(arr, start, part2 - 1);
-        }
-        if (part2 < end) {
-            quickSort(arr, part2, end);
-        }
-    }
-
-    private static int partition(int[] arr, int start, int end) {
-        int pivot = arr[(start + end) / 2];
-        while (start <= end) {
-            while (arr[start] < pivot) {
-                start++;
-            }
-            while (arr[end] > pivot) {
-                end--;
-            }
-            if (start <= end) {
-                swap(arr, start, end);
-                start++;
-                end--;
-            }
-        }
-
-        return start;
-    }
-
-    private static void swap(int[] arr, int start, int end) {
-        int tmp = arr[start];
-        arr[start] = arr[end];
-        arr[end] = tmp;
-    }
 }
